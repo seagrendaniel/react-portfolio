92081401f2a0eec32d30a644243443723433f87b
blackdanamite <seagrendaniel562@gmail.com>
2020-02-02 12:53:10 -0800

    title refactor

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -4,11 +4,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <>
+      <div className="intro-header">
+        <h2>
+          Dan Seagren
+        </h2>
+        <h4> Full Stack Engineer </h4>
+      </div>
       <div className="App">
         <header className="App-header">
           {/* <img className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and FUCK! to reload.
+        <p>
+            Edit <code>src/App.js</code> and save application to reload.
         </p>
           <a
             className="App-link"
@@ -20,6 +27,7 @@ class App extends Component {
         </a>
         </header>
       </div>
+      </>
     );
   }
 }
