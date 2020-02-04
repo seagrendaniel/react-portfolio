6d745f73344185ca8a3c37da458a522943612854
blackdanamite <seagrendaniel562@gmail.com>
2020-02-03 18:47:27 -0800

    add stand in projects with correct linking function

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -11,30 +11,64 @@ class App extends Component {
     };
   }
 
+  /* --- Lifecycle Methods --- */
+
+
+
   render() {
     return (
       <>
-      <div className="App">
-        <header className="App-header">
-          <div className="intro-header">
-        <h1>
-          Dan Seagren
-        </h1>
-        <h4> Full Stack Engineer </h4>
-      </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Daniel Seagren
-        </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header fade-in">
+            <div className="intro-header">
+              <h1>
+                Dan Seagren
+              </h1>
+              <h4> Full Stack Engineer </h4>
+            </div>
+          </header>
+          <div className="projects">
+            <h3 className="project-header"> My Projects </h3>
+            <ul className="project-list">
+              <li>
+                <a 
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Da Best Calc
+                </a>
+              </li>
+              <li> <a href="/">Da Best Calc </a></li>
+              <li> <a href="/">Da Best Calc </a></li>
+
+              <li>
+                <a
+                  href="https://summerboard.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Summerboard - The World's First eSnowboard
+                </a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
       </>
     );
   }
 }
 
 export default App;
+
+
+
+{/* <a
+className="App-link"
+href="https://reactjs.org"
+target="_blank"
+rel="noopener noreferrer"
+>
+Daniel Seagren
+</a> */}
